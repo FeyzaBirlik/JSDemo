5e4361337038029d905b6d113b2aed8187cde83e
Feyza Birlik <birlik.feyzab@gmail.com>
2020-12-15 18:12:30 +0100

    new commit

---
+++ Advance-Variables/array.js
@@ -0,0 +1,10 @@
+//const arr =[1,2,3,4,5,6];
+
+//console.log(arr[6]); //undefined deger
+
+//excersice
+//ilk bes harf array i yarat
+const ilkbes = ['a','b','c','d','e'];
+const result = ilkbes[2];
+const message = 'bu dizinin ucuncu elemani ${result}';
+console.log(message);
